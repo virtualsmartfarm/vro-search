3ad29b9bb704d02e55386f13130dc4841e0e36fe
Virtual SmartFarm <77089551+virtualsmartfarm@users.noreply.github.com>
2022-05-17 12:50:28 +1000

    Add files via upload
    Initial commit.

---
+++ search.php
@@ -0,0 +1,267 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <!-- noindex -->
+  <meta charset="iso-8859-1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta name="DCTERMS.Creator" content="Victorian Resources Online, Agriculture Victoria, Australia">
+  <meta name="DCTERMS.Publisher" content="Victorian Resources Online, Agriculture Victoria, Australia">
+  <meta name="DCTERMS.RightsHolder" content="State Government of Victoria">
+  <meta name="DCTERMS.coverage" content="Victoria">
+  <meta name="DCTERMS.title" content="copyright">
+  <meta name="DCTERMS.language" scheme="DCTERMS.RFC5646" content="en-AU">
+  <meta name="DCTERMS.type" scheme="DCTERMS.DCMIType" content="text">
+  <meta name="AGLSTERMS.aggregationLevel" content="collection">
+  <meta name="DCTERMS.identifier" scheme="DCTERMS.URI" content="https://agriculture.vic.gov.au/copyright">
+  <script>
+    var parentChild = '/vro/search';
+  </script>
+  <script>
+    (function(i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+      }, i[r].l = 1 * new Date();
+      a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-4343480-1', 'auto');
+    ga('create', 'UA-2063136-9', 'auto', {
+      'name': 'WoVGtracker'
+    });
+    ga('create', 'UA-17425154-1', 'auto', {
+      'name': 'VROUAtracker'
+    });
+    ga('create', 'UA-65891253-1', 'auto', {
+      'name': 'AGUAtracker'
+    });
+    ga('require', 'linkid', 'linkid.js');
+    ga('send', 'pageview', {
+      'page': parentChild
+    });
+    ga('WoVGtracker.send', 'pageview', {
+      'page': 'DEDJTR/agriculture.vic.gov.au' + parentChild
+    });
+    ga('VROUAtracker.send', 'pageview');
+    ga('AGUAtracker.send', 'pageview', {
+      'page': 'VRO' + parentChild
+    });
+  </script>
+
+  <link rel="stylesheet" href="https://vromedia.agriculture.vic.gov.au/assets/css/normalize.css">
+  <!-- link rel="stylesheet" href="https://vromedia.agriculture.vic.gov.au/assets/css/bootstrap.min.css"-->
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://vromedia.agriculture.vic.gov.au/assets/css/offcanvas.css">
+  <link rel="stylesheet" href="https://vromedia.agriculture.vic.gov.au/assets/css/vro.style.css">
+  <script src="https://vromedia.agriculture.vic.gov.au/assets/js/vendor/modernizr-2.8.3.min.js"></script>
+  <link rel="shortcut icon" type="image/x-icon" href="https://vromedia.agriculture.vic.gov.au/assets/graphics/favicon.ico">
+
+  
+
+  <style>
+    .vro-banner {
+      margin-left: 0;
+      margin-right: 0;
+    }
+    .navbar-form {
+      padding-left: 0;
+    }
+    .vro-search-body {}
+    /* Landscape phone width and below */
+    @media (max-width: 480px) {
+      .vro-search-body {
+        margin: -20px -10px 0 -10px;
+      }
+      .vro-navbar-brand {
+        background-position-x: 5px;
+      }
+      .vro-footer-contact,
+      .vro-footer-copyright,
+      .vro-footer-banner {
+        padding: 0 5px;
+      }
+    }
+    .vro-navbar-brand {
+      background-position-x: 5px
+    }
+    /*
+start: Funnelback Serach styles
+*/
+    .search-results {}
+    .search-results-count {
+      padding-top: 15px;
+    }
+    .search-results-pagination {
+      padding-top: 15px;
+    }
+    .search-results-list {}
+    .search-results-list-item {
+      padding-top: 25px;
+    }
+    .search-results-list-item-name {}
+    .search-results-list-item-summary {}
+    .search-results-list-item-link {
+      color: #3f6e35;
+    }
+    /*
+end: Funnelback Serach styles
+*/
+  </style>
+  <title>Search | VRO | Agriculture Victoria</title>
+</head>
+<body>
+  <nav class="navbar navbar-inverse vro-navbar">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed vro-navbar-toggle" data-toggle="offcanvas" data-target="1#navbar" aria-expanded="false" aria-controls="1navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <button type="button" class="btn btn-primary btn-xs navbar-toggle vro-fb-search-toggle" data-toggle="collapse" data-target=".vro-fb-search-wrapper">
+        <span class="glyphicon glyphicon-search vro-glyphicon-search"></span>
+      </button>
+      <div class="navbar-brand vro-navbar-brand" href="#"><span class="hidden">Victorian Resources Online | Agriculture Victoria</span></div><!-- /.navbar-brand .vro-navbar-brand -->
+    </div><!-- /.navbar-header -->
+    <div class="collapse nav navbar-nav navbar-collapse pull-right vro-fb-search-wrapper">
+      <form action="search.php" method="GET" class="navbar-form navbar-right">
+        <div class="input-group">
+          <input name="collection" value="vro-web-html" type="hidden">
+          <input required="" name="query" id="query" title="Search VRO for..." value="" accesskey="q" placeholder="Search VRO for..." class="form-control query ui-autocomplete-input" autocomplete="off" type="text">
+          <span class="input-group-btn">
+            <button type="submit" class="btn btn-primary">
+          </span>
+          <span class="glyphicon glyphicon-search"></span> Search</button>
+        </div>
+        <div class="checkbox-inline"></div>
+      </form>
+    </div><!-- /.collapse nav navbar-nav navbar-collapse pull-right vro-fb-search-wrapper -->
+  </nav>
+  <div class="row vro-banner">
+    <div class="col-sm-3">
+      <p class="hide">Your gateway to a wide range of natural resources information and associated maps</p>
+    </div>
+    <div class="col-sm-9">
+      <p class="hide">Victorian Resources Online</p>
+    </div>
+  </div>
+  <nav class="navbar">
+    <ol class="breadcrumb vro-breadcrumb">
+      <li><a href="/dpi/vro/vrosite.nsf/pages/vrohome">VRO</a></li>
+      <li><a href="https://vrosearch.agriculture.vic.gov.au/search.php">Search</a></li>
+    </ol><!-- /.breadcrumb vro-breadcrumb -->
+  </nav><!-- /.navbar -->
+  <div class="container-fluid">
+    <div class="row row-offcanvas row-offcanvas-right">
+      <div class="col-xs-6 col-sm-4 col-md-3 sidebar-offcanvas vro-sidebar-offcanvas">
+        <ol class="nav vro-nav">
+          <li><a href="http://vro.agriculture.vic.gov.au/dpi/vro/vrosite.nsf/pages/vrohome">Victorian Resources Online</a></li>
+          <li><a href="http://vro.agriculture.vic.gov.au/dpi/vro/vrosite.nsf/pages/regionalprofile">Catchment Management Regions</a></li>
+          <li><a href="http://vro.agriculture.vic.gov.au/dpi/vro/soilsurv.nsf/html/index">Soil and Land Survey Directory</a></li>
+          <li><a href="http://vro.agriculture.vic.gov.au/dpi/vro/vrosite.nsf/pages/sitemap">VRO Sitemap</a></li>
+          <li><a href="http://vro.agriculture.vic.gov.au/dpi/vro/vrosite.nsf/pages/feedback">VRO Feedback</a></li>
+        </ol>
+      </div><!-- /.col-xs-6 col-sm-4 col-md-3 sidebar-offcanvasro-sidebar-offcanvas -->
+      <div class="col-xs-12 col-sm-8 col-md-9">
+        <div class="row vro-section-banner">
+          <img src="https://vromedia.agriculture.vic.gov.au/assets/graphics/vrobnr-statewide.png" height="65" class="hidden-sm hidden-xs" alt="Victorian Resources Online - Statewide">
+        </div><!-- /.row .vro-section-banner -->
+        <!-- endnoindex -->
+        <div class="row vro-search-body">
+          <h1>Search</h1>
+          <?php
+          // The base components for our request
+          $protocol = "https://";
+          $host = "djpr-search.squiz.cloud";
+          $base_path = "/s/search.html?";
+          // Don't forget to input your collection name below
+          $default_cgi_string = "collection=vro-web-html&query=";
+          $url = $protocol . $host . $base_path;
+          // Do we have a CGI string? This is an important check!
+          if ($_GET) {
+            // Rebuild our query string - do not filter the CGI params
+            $url .= http_build_query($_GET);
+          } else {
+            // More importantly, we set the default cgi string
+            // if none is given. 
+            $url .= $default_cgi_string;
+          }
+          // Now let's init our cURL to fetch the results
+          $curl = curl_init();
+          curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $url,
+            CURLOPT_TIMEOUT => 5
+          ));
+          // Execute
+          $response = curl_exec($curl);
+          // and close.
+          curl_close($curl);
+          if ($response) {
+            // The response here contains the rendered HTML results
+            echo $response;
+          } else {
+            // You should take appropriate action here
+            echo "<p>Something went wrong</p>";
+          }
+          ?>
+          <a href="#" class="back-to-top body-text">Page top</a>
+          <hr>
+        </div><!-- /.row .vro-body -->
+      </div>
+      <!--/.col-xs-12 .col-sm-8 .col-md-9 -->
+    </div>
+    <!--/.row row-offcanvas row-offcanvas-right -->
+    <!-- noindex -->
+    <div class="row vro-footer">
+      <div class="col-md-offset-3 col-sm-offset-4 col-sm-4 col-xs-6 vro-footer-contact">
+        <p>For information about <a href="https://agriculture.vic.gov.au" data-toggle="tooltip" title="Agriculture Victoria">Agriculture Victoria</a> please contact:</p>
+        <p>Phone: <a href="tel:136186">136 186</a></p>
+        <p>National Relay Service: <a href="tel:133677">133 677</a> or <a href="https//relayservice.gov.au">relayservice.gov.au</a></p>
+        <p>Email: <a href="mailto:customer.service@ecodev.vic.gov.au">customer.service@ecodev.vic.gov.au</a></p>
+        <p>Online: <a href="https//agriculture.vic.gov.au/about-us/contact-us">Contact us</a></p>
+      </div><!-- /.col-sm-3 col-sm-offset-3 col-xs-6 vro-footer-contact -->
+      <div class="col-sm-4 col-md-5 col-xs-6 vro-footer-copyright">
+        <span class="pull-right" style="padding-right: 15px">
+          <p><a href="https//agriculture.vic.gov.au/copyright">Copyright</a></p>
+          <p><a href="https//agriculture.vic.gov.au/privacy">Privacy</a></p>
+          <p><a href="https//agriculture.vic.gov.au/disclaimer">Disclaimer</a></p>
+          <p><a href="https//agriculture.vic.gov.au/accessibility">Accessibility</a></p>
+        </span>
+      </div><!-- /.col-md-offset-4 col-sm-offset-4 col-sm-4 col-xs-6 vro-footer-contact -->
+      <div class="col-sm-8 col-sm-offset-4 col-md-offset-3 col-xs-12 vro-footer-banner">
+        <a href="//www.vic.gov.au"><img src="https://vromedia.agriculture.vic.gov.au/assets/graphics/vicgov-dedjtr-black.png" height="64" alt="State Government of Victoria logo - link to Victorian Government home" usemap="#vicgov-dedjtr-black">
+          <map name="vicgov-dedjtr-black">
+            <area shape="rect" coords="0,0,129,64" href="https://www.vic.gov.au">
+            <area shape="rect" coords="129,0,287,64" href="https://economicdevelopment.vic.gov.au">
+          </map>
+        </a>
+        <p>&copy; State of Victoria (Agriculture Victoria) 1996 - 2020.</p>
+        <a rel="license" href="https://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png"></a>
+        <p>This work, Victorian Resources Online, is licensed under a <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 licence</a>. You are free to re-use the work under that licence, on the condition that you credit the State of Victoria (Agriculture Victoria) as author, indicate if changes were made and comply with the other licence terms.</p><br>
+        <p>The licence does not apply to &#8216;branding&#8217; or some &#8216;images or photographs&#8217; that may be owned by third parties. We ask you to seek prior approval to use images using the <a href="https://vro.agriculture.vic.gov.au/dpi/vro/vrosite.nsf/pages/feedback ">VRO feedback form</a>. Access to higher quality images can also be provided on request.</p><br>
+        <p>This page was last updated on 17/05/2022.</p>
+      </div><!-- /.col-sm-5 col-sm-offset-4 col-xs-6 vro-footer-contact -->
+    </div><!-- /.row vro-footer -->
+  </div>
+  <!--/.container-fluid-->
+  <!-- start: dedjtr-caretaker.js / this includes COVID-19 -->
+  <script src="https://web5.economicdevelopment.vic.gov.au/__data/assets/js_file/0022/25195/dedjtr-caretaker.js" defer=""></script>
+  <!-- start: dedjtr-caretaker.js / this includes COVID-19 -->
+  <script>
+    window.jQuery || document.write('<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"><\/script>')
+  </script>
+  <!--script src="https://vromedia.agriculture.vic.gov.au/assets/js/vendor/bootstrap.min.js"></script-->
+  <!-- JavaScript Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <script src="https://vromedia.agriculture.vic.gov.au/assets/js/vendor/offcanvas.js"></script>
+  <script src="https://vromedia.agriculture.vic.gov.au/assets/js/plugins.js"></script>
+  <script src="https://vromedia.agriculture.vic.gov.au/assets/js/vro.cumulative.js"></script>
+</body>
+</html>
